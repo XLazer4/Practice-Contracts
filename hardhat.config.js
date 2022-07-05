8daa5e7bf5a5740a8c42da581ce88154b0e5bd03
XLazer4 <khannarahul446@gmail.com>
2022-07-05 18:14:10 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
-//require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
-require('@openzeppelin/hardhat-upgrades');
+//require('@openzeppelin/hardhat-upgrades');
 require('dotenv').config();
 
 module.exports = {
