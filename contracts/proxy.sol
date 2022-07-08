e14602835929a37b9b56ec3d897a39851dbeeda1
XLazer4 <khannarahul446@gmail.com>
2022-07-08 23:00:47 +0530

    hmm

---
+++ contracts/proxy.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract proxy is IERC20{
+    
+    constructor(){
+        
+    }
+}
