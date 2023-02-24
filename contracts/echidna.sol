c3a4e0422357b6292a06b81defcd638b0020bd68
XLazer4 <khannarahul446@gmail.com>
2023-02-24 15:19:39 +0530

    tested echidna

---
+++ contracts/echidna.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Counter {
+    uint public count;
+
+    function inc() external {
+        count += 1;
+    }
+
+    function dec() external {
+        count -= 1;
+    }
+}
+
+contract Echidna is Counter {
+    bool private pass = true;
+    uint private createdAt = block.timestamp;
+
+    function echidna_test_timestamp() public view returns (bool) {
+        return pass;
+    }
+
+    function setFail() external {
+        uint delay = 7 days;
+        require(block.timestamp >= createdAt + delay);
+        pass = false;
+    }
+
+    // function echidna_test_count() public view returns (bool) {
+    //     return count <= 5;
+    // }
+
+}
