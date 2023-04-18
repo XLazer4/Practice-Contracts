762b0985599f59de19b28d70341af01ee8c89891
XLazer4 <khannarahul446@gmail.com>
2023-04-18 16:25:55 +0530

    iterable mapping

---
+++ old contracts/SCP/iterablemapping.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+contract IterableMapping {
+    mapping(address => uint) public balances;
+    mapping(address => bool) public inserted;
+    address[] public keys;
+
+    function set(address _key, uint _val) external {
+        balances[_key] = _val;
+
+        if (!inserted[_key]) {
+            inserted[_key] = true;
+            keys.push(_key);
+        }
+    }
+}
